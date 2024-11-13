297537448f676f2c65e2d576b54f5c4c75e63a71
P. Pablo Cubells Talavera <pablocub@ucm.es>
2024-11-13 23:10:57 +0000

    cosas

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { CiClock2 } from "react-icons/ci";
 
 //create your first component
 const SecondsCounter = ({timer}) => {
@@ -8,7 +6,6 @@ const SecondsCounter = ({timer}) => {
         <div className="container-fluid">
             <div className="row text-center bg-dark text-light px-5 py-3 justify-content-center" style={{fontSize: '5rem'}}>
                 <div className="col-1 mx-3 border-3 rounded border-end border-start"><i className="fa-regular fa-clock" style={{color: '#ffffff'}}></i></div>
-                {/* <div className="col-1 px-3"><CiClock2 className="border-2 rounded border-end border-start" /></div> */}
                 <div className="col-1 mx-3 border-3 rounded border-end border-start">{Math.floor(timer/10000)%10}</div>
                 <div className="col-1 mx-3 border-3 rounded border-end border-start">{Math.floor(timer/1000)%10}</div>
                 <div className="col-1 mx-3 border-3 rounded border-end border-start">{Math.floor(timer/100)%10}</div>
